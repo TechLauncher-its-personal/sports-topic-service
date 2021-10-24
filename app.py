5dfd329b7948aec62dabe31736830e1b30497681
Anggriosutopo <u7162593@anu.edu.au>
2021-10-24 20:12:05 +0700

    Try getting database values

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def get_random():
     cur.execute(sql)
     database = [row[0] for row in cur.fetchall()]
     print(database)
+    print([article for article in myData if article not in database])
     conn.commit()
     cur.close()
     conn.close()
